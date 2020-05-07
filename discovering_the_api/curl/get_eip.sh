1bb5159ce230725742c99bfe1c67360e72e25267
cloudcoach-orange <62014296+cloudcoach-orange@users.noreply.github.com>
2020-05-07 10:49:36 +0200

    Update get_eip.sh

--- discovering_the_api/curl/get_eip.sh
+++ discovering_the_api/curl/get_eip.sh
@@ -7,7 +7,7 @@ uricall="https://$endpoint$apipath"
 
 #call to get token first
 uriToken="${OS_AUTH_URL}/auth/tokens"
-export token="$(curl -s -i -k -H "Content-Type: application/json" -X POST -d '{"auth":{"identity": {"methods":["password"], "password":{"user":{"name":"'$OS_USERNAME'","password":"'$OS_PASSWORD'","domain":{"id":"'$OS_USER_DOMAIN_ID'"}}}},"scope":{"project": {"name":"'$OS_REGION_NAME'"}}}}' $uriToken | awk '/X-Subject-Token/ {print $2}')"
+export token="$(curl -s -i -k -H "Content-Type: application/json" -X POST -d '{"auth":{"identity": {"methods":["password"], "password":{"user":{"name":"'$OS_USERNAME'","password":"'$OS_PASSWORD'","domain":{"id":"'$OS_USER_DOMAIN_ID'"}}}},"scope":{"project": {"name":"'$OS_PROJECT_NAME'"}}}}' $uriToken | awk '/X-Subject-Token/ {print $2}')"
 
 #call to API
 curl -k -H "Content-Type: application/json" -H "X-Auth-Token: ${token}" -X $method $uricall
