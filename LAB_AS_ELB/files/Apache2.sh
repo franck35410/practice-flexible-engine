e394b6d14965394cf5038918630541713aa578de
cloudcoach-orange <62014296+cloudcoach-orange@users.noreply.github.com>
2023-09-18 11:23:00 +0200

    Delete LAB_AS_ELB/files directory

--- LAB_AS_ELB/files/Apache2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Upodating the package lists
-sudo apt-get update
-
-#Apache2 installation
-sudo apt-get install apache2 -y
-sudo /etc/init.d/apache2 restart
--- LAB_AS_ELB/files/Bench.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Upodating the package lists
-sudo apt-get update
-
-#Apache benchmark installation
-sudo apt-get install apache2-utils -y
